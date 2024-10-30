51baa2541f954a14028336b466581e0a92e1613d
james2323123 <36201181+james2323123@users.noreply.github.com>
2024-10-30 15:07:48 +0800

    Update problem_pdtspl.py

--- problems/problem_pdtspl.py
+++ problems/problem_pdtspl.py
@@ -255,7 +255,8 @@ class PDPDataset(Dataset):
         self.size = size
 
         if filename is not None:
-            assert os.path.splitext(filename)[1] == '.pkl', 'file name error'
+            # does not work on windows
+            # assert os.path.splitext(filename)[1] == '.pkl', 'file name error'
             
             with open(filename, 'rb') as f:
                 data = pickle.load(f)
